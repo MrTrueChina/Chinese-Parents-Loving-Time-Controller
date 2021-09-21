58b344a43865fc2e89b17f700aaf4a2374b69ff9
Mr.ture.China <mr.true.china@gmail.com>
2021-09-21 12:02:31 +0800

    添加对社交按钮显隐的控制

---
+++ Loving-Time-Controller-Sources/LovingButtonControl.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using HarmonyLib;
+
+namespace MtC.Mod.ChineseParents.LovingTimeController
+{
+    /// <summary>
+    /// 读取玩家数据方法，可能是读档的时候用到的方法
+    /// </summary>
+    [HarmonyPatch(typeof(player_data), "read_data")]
+    public static class player_data_read_data
+    {
+        private static void Postfix()
+        {
+            // 如果 Mod 未启动则直接按照游戏原本的逻辑进行调用
+            if (!Main.enabled)
+            {
+                return;
+            }
+
+            Main.ModEntry.Logger.Log("读取玩家数据方法调用完毕");
+
+            // 如果到了约会出现回合数，则显示社交面板，否则禁用社交面板
+            if(player_data.Instance.Round_current >= Main.settings.LovingOpenRounds)
+            {
+                open_system.InstanceOpenSystem.opensystem(open_system.InstanceOpenSystem.GirlGameObject);
+            }
+            else
+            {
+                open_system.InstanceOpenSystem.GirlGameObject.SetActive(false);
+            }
+        }
+    }
+
+    /// <summary>
+    /// 每周开始时开启按钮的方法
+    /// </summary>
+    [HarmonyPatch(typeof(week_player), "system_open")]
+    public static class week_player_system_open
+    {
+        private static void Postfix()
+        {
+            // 如果 Mod 未启动则直接按照游戏原本的逻辑进行调用
+            if (!Main.enabled)
+            {
+                return;
+            }
+
+            Main.ModEntry.Logger.Log("每周开始时开启按钮的方法调用完毕");
+
+            // 如果到了约会出现回合数，则显示社交面板，否则禁用社交面板
+            if (player_data.Instance.Round_current >= Main.settings.LovingOpenRounds)
+            {
+                open_system.InstanceOpenSystem.opensystem(open_system.InstanceOpenSystem.GirlGameObject);
+            }
+            else
+            {
+                open_system.InstanceOpenSystem.GirlGameObject.SetActive(false);
+            }
+        }
+    }
+}
